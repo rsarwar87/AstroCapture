821fbac4f549c548ce276d02386a75b193b165f2
rsarwar <rsarwar>
2023-06-11 21:37:14 +0100

    cleaned up live view

--- ViewPort.hpp
+++ ViewPort.hpp
@@ -1,5 +1,9 @@
 #ifndef __ViewPort__
 #define __ViewPort__
+#include <spdlog/spdlog.h>
+
+#include <filesystem>
+#include <memory>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
@@ -7,37 +11,46 @@
 #include "AcqusitionHandler.hpp"
 #include "hello_imgui/hello_imgui.h"
 #include "imgui_md_wrapper/imgui_md_wrapper.h"
-#include "inspector.hpp"
-#include <spdlog/spdlog.h>
-#include <memory>
 #include "immvision/immvision.h"
-#include <filesystem>
+#include "inspector.hpp"
 
 using namespace ImmVision;
-class ViewPort  : public IVInspector {
+class ViewPort : public IVInspector {
  public:
-  ViewPort(std::shared_ptr<AcqManager> _acqManage)
-      : acqManage(_acqManage) {
-    spdlog::info("Initializing: {}... {}", __func__, ResourcesDir() + "/house.jpg");
-
+  ViewPort(std::shared_ptr<AcqManager> _acqManage) : acqManage(_acqManage) {
+    spdlog::info("Initializing: {}... {}", __func__,
+                 ResourcesDir() + "/house.jpg");
   }
   void gui() { guiHelp(); }
 
  private:
   std::shared_ptr<AcqManager> acqManage;
-  void guiHelp(bool show_list = false) { 
+  void guiHelp() {
     static bool is_first = false;
-    if (!is_first) FillInspector();
-    is_first = true;
-    Inspector_Show(show_list); 
+    if (!is_first) {
+      FillInspector();
+      is_first = true;
+      Inspector_Show(true);
+    } else {
+      Inspector_Show(true, &blur);
+    }
+    GuiSobelParams();
   }
-std::string ResourcesDir()
-{
+  std::string ResourcesDir() {
     std::filesystem::path this_file(__FILE__);
     return ("/home/rsarwar/workspace/AstroCapture/build/resources");
-}
-void FillInspector()
-{
+  }
+  cv::Mat* UpdateInspector() {
+    std::string zoomKey = "zk";
+    auto image = cv::imread(ResourcesDir() + "/house.jpg");
+    static cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    auto item = s_Inspector_ImagesAndParams;
+    item.Params.RefreshImage = true;
+    return &gray;
+  }
+  cv::Mat blur;
+  void FillInspector() {
     std::string zoomKey = "zk";
     auto image = cv::imread(ResourcesDir() + "/house.jpg");
     Inspector_AddImage(image, "Original", zoomKey);
@@ -46,7 +59,6 @@ void FillInspector()
     cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
     Inspector_AddImage(gray, "Gray", zoomKey);
 
-    cv::Mat blur;
     cv::GaussianBlur(gray, blur, cv::Size(), 7.);
     Inspector_AddImage(blur, "Blur", zoomKey);
 
@@ -54,8 +66,62 @@ void FillInspector()
     blur.convertTo(floatMat, CV_64FC1);
     floatMat = floatMat / 255.f;
     Inspector_AddImage(floatMat, "FloatMat", zoomKey);
+    blur = image;
+  }
+  bool GuiSobelParams() {
+    bool changed = false;
+    SobelParams& params = s_Inspector_ImagesAndParams.sParam;
+    // Blur size
+    ImGui::SetNextItemWidth(ImmApp::EmSize() * 5);
+    if (ImGui::SliderFloat("Blur size", &params.blur_size, 0.5f, 10.0f)) {
+      changed = true;
+    }
+    ImGui::SameLine();
+    ImGui::Text(" | ");
+    ImGui::SameLine();
 
-}
+    // Deriv order
+    ImGui::Text("Deriv order");
+    ImGui::SameLine();
+    for (int deriv_order = 1; deriv_order <= 4; ++deriv_order) {
+      if (ImGui::RadioButton(std::to_string(deriv_order).c_str(),
+                             params.deriv_order == deriv_order)) {
+        changed = true;
+        params.deriv_order = deriv_order;
+      }
+      ImGui::SameLine();
+    }
+
+    ImGui::Text(" | ");
+    ImGui::SameLine();
+
+    ImGui::Text("Orientation");
+    ImGui::SameLine();
+    if (ImGui::RadioButton("Horizontal",
+                           params.orientation == Orientation::Horizontal)) {
+      changed = true;
+      params.orientation = Orientation::Horizontal;
+    }
+    ImGui::SameLine();
+    if (ImGui::RadioButton("Vertical",
+                           params.orientation == Orientation::Vertical)) {
+      changed = true;
+      params.orientation = Orientation::Vertical;
+    }
+    const char *items[] = {"None", "10 fps", "20 fps", "30 fps"};
+    ImGui::SameLine();
+    ImGui::SetNextItemWidth(ImmApp::EmSize() * 5);
+    ImGui::Combo("PreviewFPS",
+                 &(s_Inspector_ImagesAndParams.targetFPS), items,
+                 IM_ARRAYSIZE(items));
+    ImGui::SameLine();
+    ImGui::SetNextItemWidth(ImmApp::EmSize() * 5);
+    ImGui::Combo("RecordingFPS",
+                 &(s_Inspector_ImagesAndParams.recordFPS), items,
+                 IM_ARRAYSIZE(items));
+
+    return changed;
+  }
 };
 
 #endif
--- inspector.hpp
+++ inspector.hpp
@@ -16,6 +16,20 @@
 #endif
 #include <spdlog/spdlog.h>
 using namespace ImmVision;
+enum class Orientation
+{
+    Horizontal,
+    Vertical
+};
+
+struct SobelParams
+{
+    float blur_size = 1.25f;
+    int deriv_order = 1;  // order of the derivative
+    int k_size = 7;  // size of the extended Sobel kernel it must be 1, 3, 5, or 7 (or -1 for Scharr)
+    Orientation orientation = Orientation::Vertical;
+};
+
 struct Inspector_ImageAndParams {
   ImageCache::KeyType id;
   std::string Label;
@@ -25,6 +39,9 @@ struct Inspector_ImageAndParams {
   cv::Point2d InitialZoomCenter = cv::Point2d();
   double InitialZoomRatio = 1.;
   bool WasSentToTextureCache = false;
+  SobelParams sParam;
+  int targetFPS = 0; 
+  int recordFPS = 0; 
 };
 
 class IVInspector {
@@ -36,13 +53,8 @@ class IVInspector {
 
   ImVec2 gInspectorImageSize;
 
-  std::vector<Inspector_ImageAndParams> s_Inspector_ImagesAndParams;
-  ImageCache::ImageTextureCache sInspectorImageTextureCache;
+  Inspector_ImageAndParams s_Inspector_ImagesAndParams;
   size_t s_Inspector_CurrentIndex = 0;
-
-  // In the inspector, we cannot rely on the ID stack, since calls to AddImages
-  // will have a different stack than when will later display the image
-  bool sDontUseIdStack = false;
 
 
   void Inspector_AddImage(const cv::Mat& image, const std::string& legend,
@@ -57,204 +69,59 @@ class IVInspector {
     params.ZoomKey = zoomKey;
     params.ColormapKey = colormapKey;
     params.ShowOptionsPanel = true;
+    params.RefreshImage = true;
 
     if (gInspectorImageSize.x > 0.f)
       params.ImageDisplaySize =
           cv::Size((int)gInspectorImageSize.x, (int)gInspectorImageSize.y);
 
     std::string label =
-        legend + "##" + std::to_string(s_Inspector_ImagesAndParams.size());
-    auto id = sInspectorImageTextureCache.GetID(label, sDontUseIdStack);
-    s_Inspector_ImagesAndParams.push_back(
-        {id, label, image.clone(), params, zoomCenter, zoomRatio});
+        legend ;
+    SobelParams sparam;
+    s_Inspector_ImagesAndParams = 
+        {0, label, image.clone(), params, zoomCenter, zoomRatio};
 
-    // bump cache
-    {
-      auto& imageAndParams = s_Inspector_ImagesAndParams.back();
-      sInspectorImageTextureCache.UpdateCache(id, imageAndParams.Image,
-                                              &imageAndParams.Params, true);
-    }
   }
 
-  void priv_Inspector_ShowImagesListbox(float width) {
-    ImGui::SetNextWindowPos(ImGui::GetCursorScreenPos());
-    int idxSuppress = -1;
-    if (ImGui::BeginListBox(
-            "##ImageList",
-            ImVec2(width - 10.f, ImGui::GetContentRegionAvail().y))) {
-      for (size_t i = 0; i < s_Inspector_ImagesAndParams.size(); ++i) {
-        ImGui::PushID(i * 3424553);
-
-        auto& imageAndParams = s_Inspector_ImagesAndParams[i];
-
-        const bool is_selected = (s_Inspector_CurrentIndex == i);
-
-        auto id = sInspectorImageTextureCache.GetID(imageAndParams.Label,
-                                                    sDontUseIdStack);
-        auto& cacheImage =
-            sInspectorImageTextureCache.GetCacheImageAndTexture(id);
-
-        ImVec2 itemSize(width - 10.f, ImGui::GetFontSize() * 3.f);
-        float imageHeight = itemSize.y - ImGui::GetTextLineHeight();
-        ImVec2 pos = ImGui::GetCursorScreenPos();
-
-        {
-          auto col = ImGui::GetStyle().Colors[ImGuiCol_Button];
-          col.x = 1.;
-          ImGui::PushStyleColor(ImGuiCol_Button, col);
-          if (ImGui::SmallButton("x")) idxSuppress = i;
-          ImGui::PopStyleColor();
-          ImGui::SameLine();
-        }
-
-        std::string id_selectable =
-            imageAndParams.Label + "##_" + std::to_string(i);
-        if (ImGui::Selectable(id_selectable.c_str(), is_selected, 0, itemSize))
-          s_Inspector_CurrentIndex = i;
-
-        float imageRatio = cacheImage.GlTexture->mImageSize.x /
-                           cacheImage.GlTexture->mImageSize.y;
-        ImVec2 image_tl(pos.x, pos.y + ImGui::GetTextLineHeight());
-        ImVec2 image_br(pos.x + imageRatio * imageHeight,
-                        image_tl.y + imageHeight);
-
-        ImGui::GetWindowDrawList()->AddImage(cacheImage.GlTexture->mImTextureId,
-                                             image_tl, image_br);
-
-        ImGui::PopID();
-      }
-      ImGui::EndListBox();
-
-      if (idxSuppress >= 0)
-        s_Inspector_ImagesAndParams.erase(s_Inspector_ImagesAndParams.begin() +
-                                          (size_t)idxSuppress);
-    }
-  };
-
-  void priv_Inspector_CleanImagesParams(const ImVec2& imageSize) {
-    for (auto& i : s_Inspector_ImagesAndParams) {
-      // Force image size
-      i.Params.ImageDisplaySize = cv::Size((int)imageSize.x, (int)imageSize.y);
-
-      // Store in texture cache
-      if (!i.WasSentToTextureCache) {
-        if (i.InitialZoomRatio > 0.) {
-          i.Params.ZoomPanMatrix = ZoomPanTransform::MakeZoomMatrix(
-              i.InitialZoomCenter, i.InitialZoomRatio,
-              i.Params.ImageDisplaySize);
-        }
-
-        sInspectorImageTextureCache.UpdateCache(i.id, i.Image, &i.Params, true);
-        i.WasSentToTextureCache = true;
-      }
-    }
-
-    // Propagate current options to hidden images
-    if (s_Inspector_CurrentIndex < s_Inspector_ImagesAndParams.size()) {
-      const auto& currentParams =
-          s_Inspector_ImagesAndParams[s_Inspector_CurrentIndex].Params;
-      for (auto& v : s_Inspector_ImagesAndParams) {
-        v.Params.ShowImageInfo = currentParams.ShowImageInfo;
-        v.Params.ShowPixelInfo = currentParams.ShowPixelInfo;
-        v.Params.ShowZoomButtons = currentParams.ShowZoomButtons;
-        v.Params.ShowOptionsPanel = currentParams.ShowOptionsPanel;
-        v.Params.ShowOptionsInTooltip = currentParams.ShowOptionsInTooltip;
-        v.Params.PanWithMouse = currentParams.PanWithMouse;
-        v.Params.ZoomWithMouseWheel = currentParams.ZoomWithMouseWheel;
-        v.Params.AddWatchedPixelOnDoubleClick =
-            currentParams.AddWatchedPixelOnDoubleClick;
-      }
-    }
-  };
-
-  ImVec2 priv_Inspector_ImageSize(float listWidth, bool showOptionsColumn) {
+  void priv_Inspector_ImageSize(bool showOptionsColumn) {
     ImVec2 imageSize;
 
     float emSize = ImGui::GetFontSize();
     float x_margin = emSize * 2.f;
     float y_margin = emSize / 3.f;
     float image_info_height = ImGui::GetFontSize() * 10.f;
-    if (!s_Inspector_ImagesAndParams.empty()) {
-      const auto& params = s_Inspector_ImagesAndParams.front().Params;
+      const auto& params = s_Inspector_ImagesAndParams.Params;
       if (!params.ShowImageInfo) image_info_height -= emSize * 1.5f;
       if (!params.ShowPixelInfo) image_info_height -= emSize * 1.5f;
-    }
     float image_options_width =
         showOptionsColumn ? ImGui::GetFontSize() * 19.f : 0.f;
     ImVec2 winSize = ImGui::GetWindowSize();
-    imageSize = ImVec2(winSize.x - listWidth - x_margin - image_options_width,
+    imageSize = ImVec2(winSize.x - x_margin - image_options_width,
                        winSize.y - y_margin - image_info_height);
     if (imageSize.x < 1.f) imageSize.x = 1.f;
     if (imageSize.y < 1.f) imageSize.y = 1.f;
 
     gInspectorImageSize = imageSize;
-    return imageSize;
+    auto& i = s_Inspector_ImagesAndParams;
+    {
+      // Force image size
+      i.Params.ImageDisplaySize = cv::Size((int)imageSize.x, (int)imageSize.y);
+    }
   };
 
-   void Inspector_Show(bool show_list = false) {
+   void Inspector_Show(bool showOptionsColumn, cv::Mat * img = nullptr) {
     ImageWidgets::s_CollapsingHeader_CacheState_Sync = true;
 
-    bool showOptionsColumn = true;
-    if (!s_Inspector_ImagesAndParams.empty()) {
-      const auto& params = s_Inspector_ImagesAndParams.front().Params;
-      if ((params.ShowOptionsInTooltip) || (!params.ShowOptionsPanel))
-        showOptionsColumn = false;
-    }
-
-    static float listWidth = ImGui::GetFontSize() * 8.5f;
-
-    ImVec2 imageSize = priv_Inspector_ImageSize(listWidth, showOptionsColumn);
-    priv_Inspector_CleanImagesParams(imageSize);
-
-
-    //
-    // First column: image list
-    //
-    if (show_list){
-      // Set column width
-      {
-        ImGui::Columns(2);
-        static bool wasWidthSet = false;
-        if (!wasWidthSet) {
-          ImGui::SetColumnWidth(0, listWidth);
-          wasWidthSet = true;
-        }
-        ImGui::Text("Image list");
-        listWidth = ImGui::GetColumnWidth(0);
-      }
-      // Show image list
-      priv_Inspector_ShowImagesListbox(listWidth);
-      ImGui::NextColumn();
-    }
-
-
-    //
-    // Second column : image
-    //
+    priv_Inspector_ImageSize(showOptionsColumn);
     {
-      if (s_Inspector_ImagesAndParams.empty())
-        s_Inspector_CurrentIndex = 0;
-      else if (s_Inspector_CurrentIndex >= s_Inspector_ImagesAndParams.size())
-        s_Inspector_CurrentIndex = s_Inspector_ImagesAndParams.size() - 1;
 
-      if (s_Inspector_CurrentIndex < s_Inspector_ImagesAndParams.size()) {
-        auto& imageAndParams =
-            s_Inspector_ImagesAndParams[s_Inspector_CurrentIndex];
-        Image(imageAndParams.Label, imageAndParams.Image,
+        auto& imageAndParams = s_Inspector_ImagesAndParams;
+        Image(imageAndParams.Label, img == nullptr ? imageAndParams.Image : *img,
               &imageAndParams.Params);
-      }
     }
-
-    if (show_list)
-      ImGui::Columns(1);
 
     ImageWidgets::s_CollapsingHeader_CacheState_Sync = false;
   }
 
-  void Inspector_ClearImages() {
-    s_Inspector_ImagesAndParams.clear();
-    sInspectorImageTextureCache.ClearImagesCache();
-    s_Inspector_CurrentIndex = 0;
-  }
 };
 #endif
