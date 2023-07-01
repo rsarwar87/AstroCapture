16ab9f1614b4403d888e22b4a60d43dae6df9aa3
rock <r.sarwar87@gmail.com>
2023-07-01 20:10:38 +0000

    increase sleep

--- AcqusitionHandler.hpp
+++ AcqusitionHandler.hpp
@@ -85,9 +85,9 @@ class AcqManager {
                     }
                     ptrS->buffer->move_tail();
                   } else
-                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-                  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                  std::this_thread::sleep_for(std::chrono::milliseconds(10));
                   if (abort_view) {
                     spdlog::info("Stopped recording");
                     writer.reset();
